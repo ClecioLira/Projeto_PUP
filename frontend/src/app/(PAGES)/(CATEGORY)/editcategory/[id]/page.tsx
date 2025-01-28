65d751cd77d959bf4c0fe7cfa491d90ee8e83b62
Clécio de Lira Alves <cclecioc.com@gmail.com>
2025-01-27 21:17:40 -0300

    feat: page edit category ok

--- frontend/src/app/(PAGES)/(CATEGORY)/editcategory/[id]/page.tsx
+++ frontend/src/app/(PAGES)/(CATEGORY)/editcategory/[id]/page.tsx
@@ -8,11 +8,14 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import { useState } from "react";
-import { createCategory } from "@/services/Category";
+import { useEffect, useState } from "react";
+import { updateCategory, getCategoryById } from "@/services/Category";
 import Link from "next/link";
+import { useRouter, useParams } from "next/navigation";
 
-const CreateCategory = () => {
+export default function EditCategory() {
+  const router = useRouter();
+  const { id } = useParams();
   const [name, setName] = useState("");
   const [image, setImage] = useState("");
 
@@ -20,23 +23,37 @@ const CreateCategory = () => {
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  useEffect(() => {
+    const fetchCategory = async () => {
+      if (typeof id === "string") {
+        try {
+          const category = await getCategoryById(id);
+          setName(category.name);
+          setImage(category.image);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    };
+    fetchCategory();
+  }, []);
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setSuccess(false);
     setError(false);
 
-    try {
-      setLoading(true);
-      await createCategory({ name, image });
-      setSuccess(true);
-    } catch (error) {
-      setError(true);
-    } finally {
-      setLoading(false);
+    if (typeof id === "string") {
+      try {
+        setLoading(true);
+        await updateCategory(id, { name, image });
+        setSuccess(true);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
     }
-
-    setName("");
-    setImage("");
   };
 
   return (
@@ -53,10 +70,11 @@ const CreateCategory = () => {
         >
           <CardContent>
             <Typography style={{marginBottom: '1rem'}} variant="h5" component="h1" align="center" gutterBottom>
-              Criar Categoria
+              Editar Categoria
             </Typography>
 
             <form
+              className="form-container"
               onSubmit={handleSubmit}
               style={{ display: "flex", flexDirection: "column", gap: "1rem" }}
             >
@@ -86,7 +104,7 @@ const CreateCategory = () => {
                 color="success"
                 variant="outlined"
               >
-                {!loading && <span>Criar</span>}
+                {!loading && <span>Editar</span>}
                 {loading && <span>Aguarde...</span>}
               </Button>
 
@@ -102,11 +120,11 @@ const CreateCategory = () => {
               </Link>
 
               {success && (
-                <Alert severity="success">Categoria criada com sucesso.</Alert>
+                <Alert severity="success">Categoria editada com sucesso.</Alert>
               )}
               {error && (
                 <Alert severity="error">
-                  Erro ao criar categoria, tente novamente mais tarde.
+                  Erro ao editar categoria, tente novamente mais tarde.
                 </Alert>
               )}
             </form>
@@ -115,6 +133,4 @@ const CreateCategory = () => {
       </div>
     </section>
   );
-};
-
-export default CreateCategory;
+}
