e5c4dd97a5d4284aa9ed2ddf371ffbb10907689c
Clécio de Lira Alves <cclecioc.com@gmail.com>
2025-02-13 17:51:36 -0300

    feat: ok

--- frontend/api/server.js
+++ frontend/api/server.js
@@ -1,9 +1,9 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("./db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(router);
+server.use("/api", router);
 
 module.exports = server;
